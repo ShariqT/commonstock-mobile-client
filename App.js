f8c740287985c4ec62587d68d989f0b310d10c42
Shariq Torres <shariq.torres@gmail.com>
2018-04-22 23:12:08 -0700

    user profile

--- App.js
+++ App.js
@@ -1,10 +1,42 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Icon } from 'native-base'
 import Home from './components/home/Home'
 import Login from './components/login/Login'
 import Signup from './components/signup/Signup'
 import Foodlist from './components/foodlist/Foodlist'
-import { StackNavigator } from 'react-navigation'
+import UserProfile from './components/userprofile/UserProfile'
+import { TabNavigator, TabBarBottom, StackNavigator } from 'react-navigation'
+
+const MainApp = TabNavigator({
+  Foodlist: {
+    screen: Foodlist
+  },
+  UserProfile: {
+    screen: UserProfile
+  }
+},{
+  navigationOptions: ({ navigation }) => ({
+    tabBarIcon: ({ focused, tintColor }) => {
+      const { routeName } = navigation.state;
+      let iconName;
+      if (routeName === 'Foodlist') {
+        iconName = 'ios-restaurant';
+      } else if (routeName === 'Settings') {
+        iconName = `ios-options${focused ? '' : '-outline'}`;
+      }
+
+      // You can return any component that you like here! We usually use an
+      // icon component from react-native-vector-icons
+      return <Icon ios='ios-thumbs-up' active />;
+    },
+  }),
+  tabBarComponent: TabBarBottom,
+  tabBarPosition: 'bottom',
+  animationEnabled: false,
+  swipeEnabled: false,
+  showLabel: true
+})
 
 export default StackNavigator({
   Home: {
@@ -17,10 +49,12 @@ export default StackNavigator({
     screen: Signup
   },
   Foodlist: {
-    screen: Foodlist
+    screen: MainApp
   }
 
 })
+
+
 
 const styles = StyleSheet.create({
   container: {
---
+++ components/userprofile/UserProfile.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container, Content, Text } from 'native-base'
+
+export default class UserProfile extends React.Component{
+    render(){
+        return(
+            <Container>
+                <Content>
+                    <Text>User Profile</Text>
+                </Content>
+            </Container>
+        )
+    }
+}
